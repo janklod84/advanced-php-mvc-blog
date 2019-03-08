08974b5546150b7424b8d4e58a9aec7d7104c4fd
JanKlod <jeanyao34@yahoo.com>
2019-03-08 23:31:54 +0500

    Html class

---
+++ vendor/System/Html.php
@@ -0,0 +1,123 @@
+<?php 
+namespace  System;
+
+
+/**
+ * @package \System\Html
+*/
+class  Html
+{
+	
+		/**
+		* Application Object
+		*
+		* @var \System\Application
+		*/
+		protected $app;
+
+
+		/**
+		* Html Title
+		*
+		* @var string
+		*/
+		private $title;
+
+
+		/**
+		* Html description
+		*
+		* @var string
+		*/
+		private $description;
+
+
+		/**
+		* Html keywords
+		*
+		* @var string
+		*/
+		private $keywords;
+
+
+		/**
+		 * Constructor
+		 *
+		 * @param \System\Application $app
+		*/
+		public function __construct(Application $app)
+		{	
+	          $this->app = $app;
+		}
+
+
+		/**
+		 * Set Title
+		 *
+		 * @param string $title
+		 * @return void
+		*/
+		public function setTitle($title)
+		{
+	        $this->title = $title;
+		}
+
+
+		/**
+		 * Get Title
+		 *
+		 * @return string
+		*/
+		public function getTitle()
+		{
+	        return $this->title;
+		}
+
+
+		/**
+		 * Set Description
+		 *
+		 * @param string $description
+		 * @return void
+		*/
+		public function setDescription($description)
+		{
+	         $this->description = $description;
+		}
+
+
+		/**
+		 * Get Description
+		 *
+		 * @return string
+		 */
+		public function getDescription()
+		{
+	         return $this->description;
+		}
+
+
+		/**
+		 * Set Keywords
+		 *
+		 * @param string $keywords
+		 * @return void
+		*/
+		public function setKeywords($keywords)
+		{
+	         $this->keywords = $keywords;
+		}
+
+
+		/**
+		 * Get Keywords
+		 *
+		 * @return string
+		*/
+		public function getKeywords()
+		{	
+	         return $this->keywords;
+		}
+
+	
+}
